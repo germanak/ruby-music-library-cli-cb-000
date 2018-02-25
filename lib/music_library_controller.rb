6ccb818795c44502db49327399bac35442ff163b
Alec German <alec_german@yahoo.com>
2018-02-25 05:58:26 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -56,7 +56,9 @@ class MusicLibraryController
     puts "Which song number would you like to play?"
     input = gets.strip.to_i
 
-    song = Song.all.sort_by{|song| song.name}.detect{|song| Song.all.index(song) == input - 1}
+    Song.all.sort_by{|song| song.name}.detect{|song| Song.all.index(song) == input - 1}
+      
+    end
 
     binding.pry
   end
